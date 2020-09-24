6356a5fe4d3510cc384dafcadfa48a310c07e076
addyxvii <addisonbennett@gmail.com>
2020-09-24 14:34:46 -0600

    removing boilerplate

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import RepoSearch from '../RepoSearch/RepoSearch';
+
+function App() {
+  return (
+    <main className="App">
+     <section className="RepoSearch">
+       <RepoSearch />
+     </section>  
+    </main>
+  );
+}
+
+export default App;
